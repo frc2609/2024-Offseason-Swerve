5138f20e5eaab95ef8a56fc31463943039fddf5c
UserC2 <aj01cars@outlook.com>
2023-11-12 21:03:24 -0500

    Log networktables & DS data

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -15,6 +17,9 @@ public class Robot extends TimedRobot {
 
   @Override
   public void robotInit() {
+    DataLogManager.start(); // log NetworkTables values
+    DriverStation.startDataLog(DataLogManager.getLog()); // log DS data
+
     m_robotContainer = new RobotContainer();
   }
 
